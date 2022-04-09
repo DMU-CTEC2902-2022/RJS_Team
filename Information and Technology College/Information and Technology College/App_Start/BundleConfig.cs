a18eef6b48fe02c37f2af9167d1e42eafd5b9735
richardeze123 <P2597140@my365.dmu.ac.uk>
2022-04-09 20:23:19 +0100

    Made changes

--- Information and Technology College/Information and Technology College/App_Start/BundleConfig.cs
+++ Information and Technology College/Information and Technology College/App_Start/BundleConfig.cs
@@ -1,4 +1,4 @@
-﻿using System.Web;
+﻿ using System.Web;
 using System.Web.Optimization;
 
 namespace Information_and_Technology_College
---
+++ Information and Technology College/Information and Technology College/Controllers/SoftwareEngineeringController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Information_and_Technology_College.Controllers
+{
+    public class SoftwareEngineeringController : Controller
+    {
+        // GET: SoftwareEngineering
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
